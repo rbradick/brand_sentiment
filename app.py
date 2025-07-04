63cbeb0dacdc0653a70f40872409eff9f1ea8cac
rbradick <rbradick@gmail.com>
2025-07-04 16:15:38 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+import os
+os.environ["STREAMLIT_SUPPRESS_CONFIG_WARNINGS"] = "true"
+os.environ["STREAMLIT_DISABLE_LOGGING"] = "1"
+os.environ["STREAMLIT_HOME"] = "/tmp"
+os.environ["STREAMLIT_TELEMETRY_ENABLED"] = "0"
+
 import streamlit as st
 from scraper.google_news import scrape_google_news
 from sentiment.analyzer import analyze_sentiment
